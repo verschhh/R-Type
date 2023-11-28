3e3a721d612c076e5aff2f84c1984e65f22092b1
Dimerci <maxime.lambert@ngine.co>
2023-11-28 16:20:24 +0100

    feat(cmake) configure CmakeList to launch project

---
+++ build/CMakeFiles/R-Type.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/R-Type.dir/main.cpp.o"
+  "CMakeFiles/R-Type.dir/main.cpp.o.d"
+  "R-Type"
+  "R-Type.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/R-Type.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

04ed8bb893a4f5eeaad07d7b43a4a45e4200e2c3
Dimerci <maxime.lambert@ngine.co>
2023-11-28 15:45:14 +0100

    feat(Doxyfile): Install Doxyfile

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** main.cpp
+*/
+
+int test(int a, int b) {
+    //Sum of a and b
+    return a + b;
+}
+
+int main(int ac, char **av) {
+    int result = test(2, 4);
+    return result;
+    //RETURN
+}

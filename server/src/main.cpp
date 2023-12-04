b4e5da95d46fef524b3f53d1e7dbe892482edba7
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-12-04 17:04:20 +0100

    chore(server main) added doc on the main function to have an exemple to follow

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,10 +7,15 @@
 
 #include <iostream>
 
+/**
+ * @brief 
+ *  This is the main function of the program. It calls the test function with
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char **argv)
 {
-    /**  **/
-
     std::cout << "Hello World" << std::endl;
     return 0;
 }

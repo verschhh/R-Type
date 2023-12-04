00f07e22ef15212fdf5cf9cc8eb898172edb6edf
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-12-04 16:59:39 +0100

    chore(server) added the first file and set up the cmake

---
+++ server/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** Server main
+*/
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    /**  **/
+
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}

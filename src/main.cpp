bf99e389c0e464e6f171e880f886abc9b67ea24a
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-12-05 15:43:27 +0100

    chore(cross_platform) modified conan and Cmake to be able to run SMFL and make a window in SMFL to try it on window

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@
  * @return The sum of the two integers.
  */
 
+#include <SFML/Graphics.hpp>
+
 int test(int a, int b) {
     return a + b;
 }
@@ -35,5 +37,29 @@ int test(int a, int b) {
 
 int main(int ac, char **av) {
     int result = test(2, 4);
+    //make a SMFL window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    //make a SFML circle
+    sf::CircleShape shape(100.f);
+    //set the shape color to green
+    shape.setFillColor(sf::Color::Green);
+    //run the program as long as the window is open
+    while (window.isOpen()) {
+        //check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            //close the window if a key is pressed or if the close button is pressed
+            if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed)
+                window.close();
+        }
+        //clear the window with black color
+        window.clear(sf::Color::Black);
+        //draw everything here
+        //window.draw(...);
+        window.draw(shape);
+        //end the current frame
+        window.display();
+    }
+
     return result;
 }

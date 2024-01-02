ded23313bcb890b23ee709a1f42de0fdf6416d3a
Léonard Verschuere <leonard.verschuere@epitech.eu>
2024-01-02 17:51:21 +0100

    chore(review comment): apply some comment that we received during the review

---
+++ Server/src/InitServer.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** RType
+** File description:
+** InitServer
+*/
+
+#include <iostream>
+#include "../include/UdpServer.hpp"
+#include "UdpServer.cpp"
+
+int InitServer(int port) {
+/**
+ * @brief the function that initialize the server side of the project
+ * @param port the port on which the server will listen
+ * @return 0 if the server is correctly launched, -1 otherwise
+ */
+    try {
+        boost::asio::io_service io_service;
+        UdpServer server(io_service, port);
+        io_service.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
+}

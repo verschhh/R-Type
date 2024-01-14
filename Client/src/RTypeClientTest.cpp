61852035edd0302f27e9be6c970865747963496a
Léonard Verschuere <leonard.verschuere@epitech.eu>
2024-01-14 14:46:21 +0100

    chore(ip) changed how the client is set up

--- Client/src/RTypeClientTest.cpp
+++ Client/src/RTypeClientTest.cpp
@@ -8,9 +8,9 @@ int main() {
     try {
         srand(time(NULL));
         // Create a server receiver
-        UDPBoostNetwork::UDPReceiver cReceiver(0, "0.0.0.0");
-        int port = (rand () % 10000) + 10000;
+        UDPBoostNetwork::UDPReceiver cReceiver((rand() % 10000) + 10000, "0.0.0.0");
         std::string receiver_ip = cReceiver.getLocalIPAddress();
+        int port = (rand () % 10000) + 10000;
         std::cout << "Receiving on " << receiver_ip << ":" << port << std::endl;
         cReceiver = UDPBoostNetwork::UDPReceiver(port, receiver_ip);
 
@@ -21,11 +21,12 @@ int main() {
 
         // Create a client sender
         UDPBoostNetwork::UDPSender cSender(17003, ip_client);
+        
         std::thread r([&] { cReceiver.receive(); });
         std::string sender_ip = cSender.get_ip();
 
         // Send a "new" message to simulate a new connection
-        cSender.send("new " + sender_ip + ":" + std::to_string(port));
+        cSender.send("new " + receiver_ip + ":" + std::to_string(port));
 
         // cReceiver.receive();
 

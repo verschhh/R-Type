cd44521878cda64f38cfbc59b4ce53808b194a56
Dimerci <maxime.lambert@ngine.co>
2024-01-14 17:50:44 +0100

    feat(vcpkg) add vcpkg json

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
+if [ ! -d "vcpkg" ]; then
+    git clone https://github.com/microsoft/vcpkg.git
+else
+    cd vcpkg
+    git pull origin master
+    cd ..
+fi
+
+cd vcpkg
+./bootstrap-vcpkg.sh
+
+./vcpkg install
+
+cd ..
+
 mkdir -p build && cd build
 cmake ..
 make && mv R-Type* ..
